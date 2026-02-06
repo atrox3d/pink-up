651c5c7bec133123d331fdd2297ec346913a658c
atrox3d <atrox3d@gmail.com>
2026-02-06 16:53:34 +0100

    add error counter

--- main.sh
+++ main.sh
@@ -11,30 +11,35 @@ source "${SCRIPTPATH}/.functions" || {
 MAIN_IP="$(cat "${SCRIPTPATH}/.secret/mainip.txt")"
 SUB_IP="$(cat "${SCRIPTPATH}/.secret/subip.txt")"
 LOOP_SLEEP=5
-MAIN_SLEEP=5
-SUB_SLEEP=5
+MAIN_ERROR_SLEEP=5
+MAIN_ERROR_COUNTER=0
+SUB_ERROR_SLEEP=5
+SUB_ERROR_COUNTER=0
 
 # main loop
 while true
 do
 	# main ip loop
 	info pinging main ip "${MAIN_IP}"
-	if ! ping -c5 "${MAIN_IP}" 2>&1 >/dev/null
+	if ! ping -c5 "${MAIN_IP}" >/dev/null 2>&1
 	then
-		error main ip error
+		MAIN_ERROR_COUNTER=$((MAIN_ERROR_COUNTER+1))
+		error main ip error count: ${MAIN_ERROR_COUNTER}
 		#send mail
-		sleep ${MAIN_SLEEP}
+		sleep ${MAIN_ERROR_SLEEP}
 		continue
 	fi
 	info main ip ok
 
 	# sub ip check
 	info pinging sub ip "${SUB_IP}"
-	if ! ping -c5 "${SUB_IP}" 2>&1 >/dev/null
+	if ! ping -c5 "${SUB_IP}" >/dev/null 2>&1
 	then
 		error sub iperror
+		SUB_ERROR_COUNTER=$((SUB_ERROR_COUNTER+1))
+		error sub ip error count: ${SUB_ERROR_COUNTER}
 		# send mail
-		sleep ${SUB_SLEEP}
+		sleep ${SUB_ERROR_SLEEP}
 		continue
 	else
 		info sub ip ok
