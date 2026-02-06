23257dd529cf67fd2aff82e084fd84668ac60ef2
atrox3d <atrox3d@gmail.com>
2026-02-06 16:41:34 +0100

    better loop

--- main.sh
+++ main.sh
@@ -10,24 +10,32 @@ source "${SCRIPTPATH}/.functions" || {
 
 MAIN_IP="$(cat "${SCRIPTPATH}/.secret/mainip.txt")"
 SUB_IP="$(cat "${SCRIPTPATH}/.secret/subip.txt")"
-LOOP_SLEEP=1
-MAIN_SLEEP=1
-SUB_SLEEP=1
+LOOP_SLEEP=5
+MAIN_SLEEP=5
+SUB_SLEEP=5
 
+# main loop
 while true
 do
+	# main ip loop
 	info pinging main ip "${MAIN_IP}"
-	while ! ping -c5 "${MAIN_IP}" 2>&1 >/dev/null
-	do
+	if ! ping -c5 "${MAIN_IP}" 2>&1 >/dev/null
+	then
 		error main ip error
+		#send mail
 		sleep ${MAIN_SLEEP}
-	done
+		continue
+	fi
 	info main ip ok
+
+	# sub ip check
 	info pinging sub ip "${SUB_IP}"
 	if ! ping -c5 "${SUB_IP}" 2>&1 >/dev/null
 	then
 		error sub iperror
+		# send mail
 		sleep ${SUB_SLEEP}
+		continue
 	else
 		info sub ip ok
 		info all ok
