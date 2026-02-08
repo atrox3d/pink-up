a6ff507df3eb396bfbe4e261b651a29453ebeb01
atrox3d <atrox3d@gmail.com>
2026-02-08 11:33:56 +0100

    remove 2nd check, add dynamic sleep time

--- main.sh
+++ main.sh
@@ -11,15 +11,28 @@ source "${SCRIPTPATH}/.functions" || {
 MAIN_IP="$(cat "${SCRIPTPATH}/.secret/mainip.txt")"
 SUB_IP="$(cat "${SCRIPTPATH}/.secret/subip.txt")"
 
-LOOP_SLEEP=5
+DEBUG=false
+if $DEBUG
+then
+	TIME_UNIT=1
+else
+	TIME_UNIT=60
+fi
 
-MAIN_ERROR_SLEEP=5
+LOOP_SLEEP=$((TIME_UNIT*30))
+
+MAIN_ERROR_SLEEP=$((TIME_UNIT*5))
 MAIN_ERROR_COUNTER=0
-MAIN_MAIL_THREADSHOLD=10
+MAIN_MAIL_RETRIES=3
 
-SUB_ERROR_SLEEP=5
+SUB_ERROR_SLEEP=$((TIME_UNIT*5))
 SUB_ERROR_COUNTER=0
-SUB_MAIL_THREADSHOLD=10
+SUB_MAIL_RETRIES=3
+
+echo ${LOOP_SLEEP}
+echo ${MAIN_ERROR_SLEEP}
+echo ${SUB_ERROR_SLEEP}
+exit
 
 # main loop
 while true
@@ -31,35 +44,37 @@ do
 		MAIN_ERROR_COUNTER=$((MAIN_ERROR_COUNTER+1))
 		error main ip error count: ${MAIN_ERROR_COUNTER}
 		#send mail
-		if [ ${MAIN_ERROR_COUNTER} -ge ${MAIN_MAIL_THREADSHOLD} ]
+		if [ ${MAIN_ERROR_COUNTER} -ge ${MAIN_MAIL_RETRIES} ]
 		then
 			info sending mail for main ip
 			MAIN_ERROR_COUNTER=0
 		fi
+		info sleeping ${MAIN_ERROR_SLEEP}
 		sleep ${MAIN_ERROR_SLEEP}
 		continue
 	fi
 	info main ip ok
 
 	# sub ip check
-	info pinging sub ip "${SUB_IP}"
-	if ! ping -c5 "${SUB_IP}" >/dev/null 2>&1
-	then
-		error sub iperror
-		SUB_ERROR_COUNTER=$((SUB_ERROR_COUNTER+1))
-		error sub ip error count: ${SUB_ERROR_COUNTER}
-		# send mail
-		if [ ${SUB_ERROR_COUNTER} -ge ${SUB_MAIL_THREADSHOLD} ]
-		then
-			info sending mail for main ip
-			SUB_ERROR_COUNTER=0
-		fi
-		sleep ${SUB_ERROR_SLEEP}
-		continue
-	else
-		info sub ip ok
-		info all ok
-	fi
+	# info pinging sub ip "${SUB_IP}"
+	# if ! ping -c5 "${SUB_IP}" >/dev/null 2>&1
+	# then
+	# 	error sub iperror
+	# 	SUB_ERROR_COUNTER=$((SUB_ERROR_COUNTER+1))
+	# 	error sub ip error count: ${SUB_ERROR_COUNTER}
+	# 	# send mail
+	# 	if [ ${SUB_ERROR_COUNTER} -ge ${SUB_MAIL_RETRIES} ]
+	# 	then
+	# 		info sending mail for main ip
+	# 		SUB_ERROR_COUNTER=0
+	# 	fi
+	# 	sleep ${SUB_ERROR_SLEEP}
+	# 	continue
+	# else
+	# 	info sub ip ok
+	# 	info all ok
+	# fi
+	info sleeping ${LOOP_SLEEP}
 	sleep ${LOOP_SLEEP}
 done
 
